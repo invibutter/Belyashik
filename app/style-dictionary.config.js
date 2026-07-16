0caa92e10ca299395c56e08fc11bc57ad2f3d4dd
Maksym Kovtun <maxi.ko.igo@gmail.com>
2026-07-16 18:21:15 +0200

    Add Style Dictionary for design token generation

---
+++ app/style-dictionary.config.js
@@ -0,0 +1,18 @@
+export default {
+  source: ['tokens/tokens.json'],
+  platforms: {
+    css: {
+      transformGroup: 'css',
+      buildPath: 'src/styles/',
+      files: [
+        {
+          destination: 'tokens.css',
+          format: 'css/variables',
+          options: {
+            outputReferences: true,
+          },
+        },
+      ],
+    },
+  },
+};
